110a29c1b7f886b3fae1149791ea856e9116a11f
Eddie Krill <scherenhaenden@gmail.com>
2021-09-28 20:00:38 +0200

    Add needed Migrations

---
+++ UoWRepo/Migrations/DeleteUnneededStuff.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using FluentMigrator;
+
+namespace UoWRepo.Migrations
+{
+    [Migration(3,"DeleteUnneededStuff")]
+    public class DeleteUnneededStuff: Migration
+    {
+        public DeleteUnneededStuff()
+        {
+           // base.ConnectionString = connectionString;
+        }
+
+        public override void Down()
+        {
+            //new MigrationContext()
+
+        }
+
+        public override void Up()
+        {
+            TryDelete("Banners");
+            TryDelete("Div_configuration");
+            TryDelete("divofpage");
+            TryDelete("divOrder");
+            TryDelete("ObjectOfpages");
+            TryDelete("ObjectTypes");
+            TryDelete("page");
+            TryDelete("page_configuration");
+            TryDelete("tempDivtable");
+            TryDelete("TextItems");
+            TryDelete("VersionInfo");
+            TryDelete("Widgets");
+            TryDelete("tableuptodate");
+        }
+
+        public void TryDelete(string nameOfTable)
+        {
+            try
+            {
+                Delete.Table(nameOfTable);
+            }
+            catch (Exception e)
+            {
+            }
+        }
+    }
+}
