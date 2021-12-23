8d8c3ff8eb232af8caa9054e47fcf7f78328dc1d
Eddie Krill <scherenhaenden@gmail.com>
2021-12-23 11:31:53 +0100

    Changes:
    - Create implementatins

--- UoWRepo/Core/BaseDomain/INewsEtty.cs
+++ UoWRepo/Core/BaseDomain/INewsEtty.cs
@@ -1,6 +1,32 @@
+using System;
+
 namespace UoWRepo.Core.BaseDomain;
 
-public interface INewsEtty
+public interface INewsEtty: ITEntity
 {
-    
+    public int UserIdOwner { get; set; }
+
+    public string Newstittel { get; set; }
+
+    public string Newscontent { get; set; }
+
+    public DateTime Newscreateddate { get; set; }
+
+    public int Newspermission { get; set; }
+
+    public int Newschangedbyid { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public int Publicationtype { get; set; }
+
+    public int Galleryid { get; set; }
+
+    public string Newspresentation { get; set; }
+
+    public DateTime Publicationdate { get; set; }
+
+    public string Titleforurl { get; set; }
+
+    public int HashtagsNewsId { get; set; }
 }
