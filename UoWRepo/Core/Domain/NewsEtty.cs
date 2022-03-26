1bc961868d3bce630e2ccd7e38298ae582ddc1e1
Eddie Krill <scherenhaenden@gmail.com>
2022-03-26 12:14:53 +0100

    Update domain

--- UoWRepo/Core/Domain/NewsEtty.cs
+++ UoWRepo/Core/Domain/NewsEtty.cs
@@ -55,7 +55,7 @@ namespace UoWRepo.Core.Domain
         public int HashtagsNewsId { get; set; }
         
         [Column(Name = "ArticleVersion"), Nullable]
-        public int ArticleVersion { get; set; }
+        public string? ArticleVersion { get; set; }
 
     }
 }
