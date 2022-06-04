3e12cee14b444ba50b0205c7f9c8311e2205a10a
scherenhaenden <scherenhaenden@gmail.com>
2022-06-04 11:25:17 +0200

    Drafting

---
+++ UoWRepo/Core/Configuration/IBaseContext.cs
@@ -0,0 +1,30 @@
+using LinqToDB;
+using LinqToDB;
+using LinqToDB.DataProvider;
+using UoWRepo.Core.Domain;
+
+namespace UoWRepo.Core.Configuration;
+
+public interface INews
+{
+    
+}
+
+public interface IITableSet<ToutTableBase>
+{
+    
+}
+
+public interface IBaseContext<IITableSet>
+{
+    public IITableSet<INews> tb_news { get; set; }
+}
+
+
+public class SimpleContext: LinqToDB.Data.DataConnection, IBaseContext<ITable>
+{
+    
+
+}
+
+
