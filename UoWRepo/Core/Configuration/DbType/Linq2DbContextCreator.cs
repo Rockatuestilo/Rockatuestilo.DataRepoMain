307469e5961f528ae3dad5a47d73dd256ff574d5
scherenhaenden <scherenhaenden@gmail.com>
2023-03-17 09:23:36 +0100

    Add posibilities for different DBS

---
+++ UoWRepo/Core/Configuration/DbType/Linq2DbContextCreator.cs
@@ -0,0 +1,22 @@
+namespace UoWRepo.Core.Configuration.DbType;
+
+public class Linq2DbContextCreator
+{
+    public Linq2DbContextCreator()
+    {
+        
+    }
+    
+    public Linq2DbContext CreateLinq2DbContext(LinqDatabaseType linqDatabaseType, string connectionString)
+    {
+        switch (linqDatabaseType)
+        {
+            case LinqDatabaseType.SQLite:
+                return new Linq2DbContext ("System.Data.SQLite", connectionString);
+            case LinqDatabaseType.MySQL:
+                return new Linq2DbContext ("MySql.Data.MySqlClient", connectionString);
+            default:
+                return new Linq2DbContext ("MySql.Data.MySqlClient", connectionString);
+        }
+    }
+}
