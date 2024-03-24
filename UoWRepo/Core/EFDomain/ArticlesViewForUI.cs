88c974e9f6021760c7ae4eb9a2030db6c8690be5
scherenhaenden <scherenhaenden@gmail.com>
2024-03-24 16:49:27 +0100

    Add validation annotations to ArticlesViewForUi model

--- UoWRepo/Core/EFDomain/ArticlesViewForUI.cs
+++ UoWRepo/Core/EFDomain/ArticlesViewForUI.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using UoWRepo.Core.BaseDomain;
 
 namespace UoWRepo.Core.EFDomain;
 
 [Table("ArticlesViewForUI")]
-public class ArticlesViewForUI : TEntity, IBaseTEntity
+public class ArticlesViewForUi : TEntity,IArticlesViewForUi, ITEntity
 {
-    [Column("ArticleId")] public int ArticleId { get; set; }
+    [Range(0, int.MaxValue, ErrorMessage = "The value for ArticleId must be greater than -1.")]
+    [Column("ArticleId")] 
+    public int ArticleId { get; set; }
 
     [Column("UIString")] public string UiString { get; set; } = null!;
 
