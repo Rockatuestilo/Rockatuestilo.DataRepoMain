99d08880f67655fc239cfeb0432969676ce3a2da
scherenhaenden <scherenhaenden@gmail.com>
2024-06-20 12:07:34 +0200

    Enhance ContextCreator with EF Core setups
    Add methods to ContextCreator for generating EF Core contexts tailored to specific database types such as InMemory, SQLite, and MySQL, expanding our testing capabilities.

--- Rockatuestilo.DataRepoMain/Rockatuestilo.DataRepoMain.Tests/Tools/Contexts/ContextCreator.cs
+++ Rockatuestilo.DataRepoMain/Rockatuestilo.DataRepoMain.Tests/Tools/Contexts/ContextCreator.cs
@@ -1,6 +1,25 @@
+using Rockatuestilo.DataRepoMain.Tests.DbInit;
+using UoWRepo.Persistence.UnitiesOfWork;
+
 namespace Rockatuestilo.DataRepoMain.Tests.Tools.Contexts;
 
 public class ContextCreator
 {
+    public static IUnitOfWorkEf CreateContextEFCoreInMemory()
+    {
+        var value = new ContextGenerator().CreateInMemory();
+        return new UnityOfWorkEf(value);
+    }
     
+    public static IUnitOfWorkEf CreateContextEFCoreInSqlite(string fileName = "test.db")
+    {
+        var value = new ContextGenerator().CreateInSqlite(fileName);
+        return new UnityOfWorkEf(value);
+    }
+    
+    public static IUnitOfWorkEf CreateContextEFCoreInMysql(string connectionString)
+    {
+        var value = new ContextGenerator().CreateInMysql(connectionString);
+        return new UnityOfWorkEf(value);
+    }
 }
